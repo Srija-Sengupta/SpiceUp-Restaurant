74891e6ed11c507a691db4542f680b298a50caad
Srija-Sengupta <srijasengupta1908@gmail.com>
2025-07-05 18:41:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('reservationForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Reservation successful! We look forward to welcoming you at SpiceUP.');
+});
+
